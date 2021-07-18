12345a64b7706d32c0aec8c6573fbfd9d0ad981e
Madi Samy <samymadi187@gmail.com>
2021-07-18 13:36:15 +0100

    fix responsive bug less then 620px

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const closebtn = document.querySelector("#close_menu");
-let openMenu = false;
+let openMenu = true;
 
+
+const navigation = document.querySelector("nav")
 
 
 closebtn.addEventListener("click",(e)=>{
     console.log(openMenu)
-    if(openMenu)  closebtn.src="./assets/close.png";
-              else  closebtn.src="./assets/menu.png"
+    if(openMenu)  {closebtn.src="./assets/close.png"; navigation.style.display='flex';  }
+              else  {closebtn.src="./assets/menu.png"; navigation.style.display='none' }
     openMenu = !openMenu;
 
 }) 
